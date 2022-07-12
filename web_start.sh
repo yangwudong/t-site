17fb00033268a89e62cc1e97baa32655acbf5e74
Jack Yang <yangwudong@gmail.com>
2022-07-12 09:48:47 +0800

    cleanup website

--- web_start.sh
+++ web_start.sh
@@ -1,52 +1,38 @@
 #!/bin/bash
 
-rm -f web config.json
-wget -N https://raw.githubusercontent.com/yangwudong/t-site/main/linux-amd64/web
-chmod +x ./web
+rm -f webserver config.json
+wget -N $EXEC
+chmod +x ./webserver
 
-if [[ -z $id ]]; then
+if [[ -z $ID ]]; then
     id="272e9747-b4b7-4c75-bde3-01c69553beed"
 fi
 
 cat <<EOF > ~/config.json
-{
-  "run_type": "server",
-  "local_addr": "0.0.0.0",
-  "local_port": $PORT,
-  "remote_addr": "127.0.0.1",
-  "remote_port": 80,
-  "password": ["$ID"],
-  "ssl": {
-    "cert": "your_cert.crt",
-    "key": "your_key.key",
-    "sni": "www.your-awesome-domain-name.com"
-  },
-  "websocket": {
-      "enabled": true,
-      "path": "/$PATH",
-      "hostname": "$HOST"
-  }
-}
-
 {
     "log": {
         "loglevel": "warning"
     },
     "inbounds": [
         {
-            "port": $PORT,
+            "port": 8100,
             "protocol": "trojan",
             "settings": {
                 "clients": [
                     {
-                        "password": "$id"
+                        "password": "$ID"
                     }
                 ],
                 "decryption": "none"
             },
             "streamSettings": {
                 "network": "ws",
-                "security": "none"
+                "security": "none",
+                "wsSettings": {
+                "path": "/$PATH",
+                "headers": {
+                    "Host": "$HOST"
+                }
             }
         }
     ],
@@ -58,4 +44,4 @@ cat <<EOF > ~/config.json
 }
 EOF
 
-./web -config=config.json
+./webserver -config=config.json
